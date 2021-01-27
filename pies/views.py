68bbe449eec11f6432f60a164e88f3dd05658f6f
Penelope Jungreis <penelope.jungreis@gmail.com>
2021-01-27 11:23:41 +0000

    added pies index and show controllers

--- pies/views.py
+++ pies/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.exceptions import NotFound
 
-# Create your views here.
+# Un-comment the below when user authentication is ready:
+# from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+from .models import Pie 
+from .serializers.common import PieSerializer
+
+class PieListView(APIView):
+    """ View for GET and POST requests to 'api/pies/' """
+
+    # GET request for all pies index page
+    def get(self, _request):
+        # GET all the pies
+        pies = Pie.objects.all()
+        # Serialize all the pies
+        serialized_pies = PieSerializer(pies, many=True)
+        return Response(serialized_pies.data, status=status.HTTP_200_OK)
+
+class PieDetailView(APIView):
+    """ View for GET and PUT and DELETE requests to 'api/pies/pk/' """
+    def get_pie(self, pk):
+        """ Checks if pie PK exists """
+        try:
+            return Pie.objects.get(pk=pk)
+        except Pie.DoesNotExist:
+            raise NotFound()
+    
+    def get(self, _request, pk):
+        pie = self.get_pie(pk=pk)
+        serialized_pie = PieSerializer(pie)
+        return Response(serialized_pie.data, status=status.HTTP_200_OK)
+        
