6af970449fbcd32a6fedff6c575a5996dd8cd37a
Penelope Jungreis <penelope.jungreis@gmail.com>
2021-01-28 16:32:57 +0000

    created links for nav bar

--- client/src/components/common/Nav.js
+++ client/src/components/common/Nav.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Nav() {
   return (
-    <main>
-      <p>I am the nav bar</p>
-    </main>
+    <nav>
+      <Link to="/">
+        <p><strong>PIEKEA</strong></p>
+      </Link>
+      <Link to="/pies">
+        <p>All Pies</p>
+      </Link>
+      <Link to="/profile">
+        <p>My Account</p>
+      </Link>
+      <Link to="/basket">
+        <p>Checkout</p>
+      </Link>
+      <Link to="/register">
+        <p>Register</p>
+      </Link>
+      <Link to="/login">
+        <p>Login</p>
+      </Link>
+    </nav>
   )
 }
 
