64812507eeaa88653ddeb3776bf3ce41a78b9acc
Edwyn <edwyn.3000@btinternet.com>
2021-01-29 11:11:40 +0000

    login

---
+++ client/utils/useForm.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function useForm(initialState) {
+  const [formdata, setFormdata] = React.useState(initialState)
+  const [errors, setErrors] = React.useState(initialState)
+  const handleChange = event => {
+    const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value
+    const nextState = { ...formdata, [event.target.name]: value }
+    const nextErrorState = { ...errors, [event.target.name]: '' }
+    setFormdata(nextState)
+    setErrors(nextErrorState)
+  }
+
+  return {
+    formdata,
+    setFormdata,
+    errors,
+    handleChange,
+    setErrors
+  }
+}
+export default useForm
